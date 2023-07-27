6174bbfc28a195a434186392cbf294ed5b7c2d5d
= <Manas09rai@gmail.com>
2023-07-27 10:40:28 -0400

    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaginationService } from './pagination.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pagination_exercise';
+  data: any;
+  totalPages: number = 0;
+  selectedInterval: number = 5;
+  singlePage: number = 1;
+  iteration_arr: any = []
+
+
+  constructor(private paginationService: PaginationService) {
+
+
+  }
+
+  ngOnInit(): void {
+    this.paginationService.getData().subscribe((data) => {
+      this.data = data
+      this.totalPages = this.data.length / this.selectedInterval
+    })
+
+  }
+
+  restart() {
+    this.singlePage = 1
+  }
+
+  finalInterval() {
+    this.singlePage = this.data.length / this.selectedInterval
+  }
+
+  nextInterval() {
+    if (this.singlePage * this.selectedInterval - this.selectedInterval + 1 <= 200 - this.selectedInterval) {
+      this.singlePage += 1;
+    }
+  }
+
+  previousInterval() {
+    if (this.singlePage * this.selectedInterval - this.selectedInterval + 1 >= this.selectedInterval) {
+      this.singlePage -= 1
+    }
+  }
+
+  resetVals() {
+    this.singlePage = 1;
+    this.totalPages = this.data.length / this.selectedInterval;
+    for (let index = 1; index <= this.totalPages; index++) {
+      this.iteration_arr.push(index);
+    }
+  }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PaginationService } from './pagination.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PaginationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
